fc2350e0442c17c519b020e99846143b37355a85
Rayna MacFarlane <rmacfarlane43@gmail.com>
2022-09-21 09:33:37 -0700

    Call three different array methods

--- arrays.js
+++ arrays.js
@@ -3,3 +3,15 @@ var breakfastItems = ['oatmeal', 'cereal','toast', 'blueberries'];
 var ticketPrices = [15, 30, 45, 60];
 
 var correctAnswer = [ (1 < 2), (5 < 7), (5 < 3), (10 < 7)];
+
+// This method will remove the last element from the end
+
+breakfastItems.pop();
+
+// This method will method only include the numbers at index 1 and 2
+
+ticketPrices.slice();
+
+// This method will remove the first element from the beginning of the array
+
+correctAnswer.shift();
